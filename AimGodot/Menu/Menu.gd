34814f62c27b26f4bc540e496a1f34ce7543ab04
Rob <roafan@ttu.ee>
2023-02-28 00:08:35 +0200

    Uuendatud

---
+++ AimGodot/Menu/Menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _ready() -> void:
+	OS.window_fullscreen = true
+
+
+
+func _on_Fun_pressed():
+	get_tree().change_scene("res://Fun/Fun.tscn")
+
+
+func _on_Speed_pressed():
+	get_tree().change_scene("res://Fun/Main2.tscn")
+
+
+func _on_Hard_pressed():
+	get_tree().change_scene("res://Hard/Hard.tscn")
